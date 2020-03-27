bc6d628fb1de5ba08682a58804bb35b3f5d8f8ec
Doyal-N <coin20@protonmail.com>
2020-03-28 00:43:01 +0500

    refact

--- script/main.js
+++ script/main.js
@@ -1,19 +1,17 @@
 let money = 50000;
-let income = 'анимация';
-let addExpenses = ('programms, food, taxi, gas'); 
-let deposit = true;
-let mission = 108000;
-let period = 7;
+    income = 'анимация';
+    addExpenses = ('programms, food, taxi, gas'); 
+    deposit = true;
+    mission = 108000;
+    period = 7;
+    budgetDay = (money / 30);
+
+
+
 
 console.log(typeof(money), typeof(income), typeof(deposit));
-
 console.log(addExpenses.length);
-
-console.log( 'Период равен ' + period + ' месяцев');
-
+console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
-
 console.log(addExpenses.toLowerCase().split(', '));
-
-let budgetDay = (money / 30);
 console.log(budgetDay);
