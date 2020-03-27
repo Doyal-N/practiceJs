55ed2039b37e1eee1909d71e7cf374041abe8c15
Doyal-N <coin20@protonmail.com>
2020-03-27 21:37:14 +0500

    Второй урок (обязательная часть)

--- script/main.js
+++ script/main.js
@@ -1,10 +1,19 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 50000;
+let income = 'анимация';
+let addExpenses = ('programms, food, taxi, gas'); 
+let deposit = true;
+let mission = 108000;
+let period = 7;
 
-alert('Новое сообщение');
+console.log(typeof(money), typeof(income), typeof(deposit));
 
-console.log('Первый урок');
+console.log(addExpenses.length);
+
+console.log( 'Период равен ' + period + ' месяцев');
+
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = (money / 30);
+console.log(budgetDay);
