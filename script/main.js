3ac01df082a05b46321fc044462efc3cf3c301d7
Doyal-N <coin20@protonmail.com>
2020-03-30 14:18:38 +0500

    add lesson_03

--- script/main.js
+++ script/main.js
@@ -1,3 +1,32 @@
+'use strict';
+
+let money = prompt('Ваш месячный доход?');
+console.log(money);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*вводим переменные
 let money = 50000;
     income = 'анимация';
     addExpenses = ('programms, food, taxi, gas'); 
@@ -6,12 +35,10 @@ let money = 50000;
     period = 7;
     budgetDay = (money / 30);
 
-
-
-
+//выводим в консоль
 console.log(typeof(money), typeof(income), typeof(deposit));
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
 console.log(addExpenses.toLowerCase().split(', '));
-console.log(budgetDay);
+console.log(budgetDay);*/
