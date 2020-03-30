50406deeb309e34468e56c7d99080dc2250bf525
Doyal-N <coin20@protonmail.com>
2020-03-30 20:08:16 +0500

    lesson_03

--- script/main.js
+++ script/main.js
@@ -1,44 +1,49 @@
 'use strict';
 
-let money = prompt('Ваш месячный доход?');
-console.log(money);
+//операции с переменными и получение данных
+let money = +prompt('Ваш месячный доход?'),
+    income = 'анимация',
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    mission = 108000,
+    expenses1 = prompt('Введите обязательную статью расходов?'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+    expenses2 = prompt('Введите обязательную статью расходов?'),
+    amount2 = +prompt('Во сколько это обойдется?'),
+    budgetMonth = money - (amount1 + amount2),
+    period = Math.round(mission / budgetMonth),
+    budgetDay = Math.trunc(budgetMonth / 30);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*вводим переменные
-let money = 50000;
-    income = 'анимация';
-    addExpenses = ('programms, food, taxi, gas'); 
-    deposit = true;
-    mission = 108000;
-    period = 7;
-    budgetDay = (money / 30);
-
-//выводим в консоль
+//вывод в консоль
 console.log(typeof(money), typeof(income), typeof(deposit));
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
 console.log(addExpenses.toLowerCase().split(', '));
-console.log(budgetDay);*/
+console.log('Бюджет на месяц ' + budgetMonth);
+console.log('Цель будет достигнута за: ' + period + ' месяцев');
+console.log('Бюджет на день: ' + budgetDay);
+
+//ветвления
+if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 || budgetDay === 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+    console.log('Что-то пошло не так');
+}
+
+
+
+
+
+
+
+
+
+    
+
+
+
