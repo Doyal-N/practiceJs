d2954650abcdc3cd0fadc6ed7ff0525b7b53bbb8
Doyal-N <coin20@protonmail.com>
2020-03-31 12:38:33 +0500

    lesson_03_hard

--- script/main.js
+++ script/main.js
@@ -1,49 +1,15 @@
 'use strict';
 
-//операции с переменными и получение данных
-let money = +prompt('Ваш месячный доход?'),
-    income = 'анимация',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    mission = 108000,
-    expenses1 = prompt('Введите обязательную статью расходов?'),
-    amount1 = +prompt('Во сколько это обойдется?'),
-    expenses2 = prompt('Введите обязательную статью расходов?'),
-    amount2 = +prompt('Во сколько это обойдется?'),
-    budgetMonth = money - (amount1 + amount2),
-    period = Math.round(mission / budgetMonth),
-    budgetDay = Math.trunc(budgetMonth / 30);
+//вводим переменные (необязательный шаг)
+let lang,
+ru_days = ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
+en_days = ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'];
 
-//ветвления
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600 || budgetDay === 0) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('Что-то пошло не так');
+//условия через if
+if (lang == 'ru') {
+    console.log(ru_days);
+} else if (lang == 'en') {
+    console.log(en_days);
 }
 
-//вывод в консоль
-console.log(typeof(money), typeof(income), typeof(deposit));
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
-console.log(addExpenses.toLowerCase().split(', '));
-console.log('Бюджет на месяц ' + budgetMonth);
-console.log('Цель будет достигнута за: ' + period + ' месяцев');
-console.log('Бюджет на день: ' + budgetDay);
-
-
-
-
-
-
-
-
-
-    
-
-
-
+//условия через switch
