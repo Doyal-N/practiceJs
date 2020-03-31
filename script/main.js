411012979b1fe976d9309b133d1bf16acb1c2e66
Doyal-N <coin20@protonmail.com>
2020-03-31 20:40:45 +0500

    fix mistakes

--- script/main.js
+++ script/main.js
@@ -1,43 +1,36 @@
 'use strict';
 
 //вводим переменные (необязательный шаг)
-let lang,
-ru_days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+let ru_days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
 en_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+//многомерный массив
+let lang = {
+    'ru': ru_days,
+    'en': en_days
+}
+console.log(lang['ru']);
 
 //условия через if
 if (lang == 'ru') {
     console.log(ru_days);
 } else if (lang == 'en') {
     console.log(en_days);
-} 
+}
 
 //условия через switch
 switch (lang) {
     case 'ru':
-        console.log(ru_days);        
+        console.log(ru_days);
         break;
     case 'en':
         console.log(en_days);
         break;
     default:
-        console.log('Выберите язык');    
+        console.log('Выберите язык');
 }
 
-//многомерный массив
-lang = [
-    [ru_days],
-    [en_days]
-];
-
-console.log(lang[0]);
-
 //тернарные операторы
-let namePerson = 'Герман';
-namePerson === ('Артем') ? console.log('Директор')
-: namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
- 
-
-
-
-
+let namePerson = 'Артем';
+namePerson === ('Артем') ? console.log('Директор') :
+namePerson === 'Максим' ? console.log('Преподаватель') : console.log('Студент');
