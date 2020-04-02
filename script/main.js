aaa1f84225b0272614633adc0513652618286ff1
Doyal-N <coin20@protonmail.com>
2020-04-02 14:46:03 +0500

    lesson_04

--- script/main.js
+++ script/main.js
@@ -10,40 +10,38 @@ let money = +prompt('Ваш месячный доход?'),
     amount1 = +prompt('Во сколько это обойдется?'),
     expenses2 = prompt('Введите обязательную статью расходов?'),
     amount2 = +prompt('Во сколько это обойдется?'),
-    budgetMonth = money - (amount1 + amount2),
-    period = Math.round(mission / budgetMonth),
-    budgetDay = Math.trunc(budgetMonth / 30);
+    accumulatedMonth = function getAccumulatedMonth(money, amount1, amount2) {
+        return (money - (amount1 + amount2));
+    }    
+    period = Math.round(mission / accumulatedMonth),
+    budgetDay = Math.trunc(accumulatedMonth / 30);
+ 
+//работа с функциями
+function getExpensesMonth(amount1, amount2) {
+    return (amount1 + amount2);
+}
+
+function getTargetMonth(accumulatedMonth, period) {
+    return (accumulatedMonth / period);
+}
 
 //ветвления
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600 || budgetDay === 0) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('Что-то пошло не так');
+function getStatusIncome(budgetDay) {
+    if (budgetDay >= 1200) {
+        console.log('У вас высокий уровень дохода');
+    } else if (budgetDay >= 600) {
+        console.log('У вас средний уровень дохода');
+    } else if (budgetDay < 600 || budgetDay === 0) {
+        console.log('К сожалению у вас уровень дохода ниже среднего');
+    } else {
+        console.log('Что-то пошло не так');
+    }
 }
 
 //вывод в консоль
-console.log(typeof(money), typeof(income), typeof(deposit));
-console.log(addExpenses.length);
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+console.log(typeof (money), typeof (income), typeof (deposit));
+console.log(getExpensesMonth(amount1, amount2));
 console.log(addExpenses.toLowerCase().split(', '));
-console.log('Бюджет на месяц ' + budgetMonth);
-console.log('Цель будет достигнута за: ' + period + ' месяцев');
+console.log('Срок достижения: ' + getTargetMonth(accumulatedMonth, period));
 console.log('Бюджет на день: ' + budgetDay);
-
-
-
-
-
-
-
-
-
-    
-
-
-
+console.log(getStatusIncome(budgetDay)); 
