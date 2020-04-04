710db3f51300d64dd450addfbae6cc1e2cdf221a
Doyal-N <coin20@protonmail.com>
2020-04-04 12:13:31 +0500

    two

--- script/main.js
+++ script/main.js
@@ -1,13 +1,22 @@
 'use strict';
 
-//функция
+/*/функция
 const transformText = (str) => {
   if (typeof(str)  !== 'string') {
-    alert('Данные не строка')
+    return('Данные не строка')
   } else if (typeof(str) === 'string' && str.length > 30) {
     return str.trim().substring(0, 30) + "..."
   } else {
     return str.trim()
   }
+}*/
+
+const transformText = (str) => {
+  typeof(str) === 'string' && str.length > 30 ? alert(str.trim().substring(0, 30) + "...") : alert(str.trim());
+  typeof str !== 'string' ? alert('Данные не строка') : alert(str.trim())
+
 }
+
+transformText("Орган ЗАГС, где ранее граждане оформляли брак, может развести их при соблюдении условий.");
+
 
