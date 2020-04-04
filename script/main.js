6396d035440084aca24f43343576821d41da922f
Doyal-N <coin20@protonmail.com>
2020-04-04 08:22:15 +0500

    two if

--- script/main.js
+++ script/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
+//функция
+const transformText = function(str) {
+  if (typeof(str)  !== 'string') {
+    alert('Данные не строка')
+  } else if (typeof(str) === 'string') {
+     console.log(str.trim());
+  }
+}
 
+transformText('                   ну на конец то              ');
