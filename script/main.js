1f9f409a1e928cade817f6574da8cddaa256d4c1
Doyal-N <coin20@protonmail.com>
2020-04-05 22:24:11 +0500

    lesson_05

--- script/main.js
+++ script/main.js
@@ -1,28 +1,53 @@
 'use strict';
 
+const isNum = function(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+};
+
 //операции с переменными и получение данных
-let money = +prompt('Ваш месячный доход?'),
+let money,
     income = 'анимация',
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
     deposit = confirm('Есть ли у вас депозит в банке?'),
     mission = 108000,
-    expenses1 = prompt('Введите обязательную статью расходов?'),
-    amount1 = +prompt('Во сколько это обойдется?'),
-    expenses2 = prompt('Введите обязательную статью расходов?'),
-    amount2 = +prompt('Во сколько это обойдется?'),
     accumulatedMonth;
 
+let start = function() {
+    money = prompt('Ваш месячный доход?');
+
+    while (!isNum(money)) {
+        money = prompt('Ваш месячный доход?');
+    }
+};
+
+start();
+   
 //работа с функциями
 const showTypeOf = function(data) {
-    return typeof(data)
+    return typeof (data)
 };
+
+let expenses = [];
 
 const getExpensesMonth = function() {
-    return (amount1 + amount2);
+
+    let sum = 0;
+    
+    for (let i = 0; i < 2; i++) {
+      
+     expenses[i] = prompt('Введите обязательную статью расходов?');    
+
+     sum += +prompt('Во сколько это обойдется?');
+
+    };
+    console.log(sum);
+    return sum;
 };
 
+let expensesAmount = getExpensesMonth();
+
 const getAccumulatedMonth = function() {
-    return (money - (amount1 + amount2));
+    return money - expensesAmount;
 };
 
 accumulatedMonth = getAccumulatedMonth();
@@ -50,7 +75,6 @@ const getStatusIncome = function() {
 console.log(showTypeOf(money));
 console.log(showTypeOf(income));
 console.log(showTypeOf(deposit));
-console.log(getExpensesMonth());
 console.log(addExpenses.toLowerCase().split(', '));
 console.log(Math.round(getTargetMonth()));
 console.log(budgetDay);
