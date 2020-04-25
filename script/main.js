e4f2c92fa0036ed8b9239be60ff4d48cb17eaf88
Doyal-N <coin20@protonmail.com>
2020-04-25 13:38:06 +0500

    lesson_13

--- script/main.js
+++ script/main.js
@@ -6,20 +6,12 @@ let todoControl = document.querySelector('.todo-control'),
     todoCompleted = document.querySelector('.todo-completed'),
     headerBtn = document.getElementById('add');
 
+let todoData = [];     
 
-
-const todoData = [
-  {
-   value: 'Погулять с собакой',
-   completed: false
-
-  },
-
-];    
-
+//функция рендер уже добавленных дел и  состояний
 const render = function() {
- todoList.textContent = '';
- todoCompleted.textContent = '';
+   todoList.textContent = '';
+   todoCompleted.textContent = '';
 
   todoData.forEach(function(item){
     const li = document.createElement('li');
@@ -37,13 +29,12 @@ const render = function() {
       todoList.append(li);
     }
 
- 
     const btnTodoComplete = li.querySelector('.todo-complete');
     btnTodoComplete.addEventListener('click', function(){
       item.completed = !item.completed;
       render();
     })
-    
+  //удаляем дело по клике на корзину  
     const btnRemove = li.querySelector('.todo-remove');
     btnRemove.addEventListener('click', function(){
       li.remove();
@@ -59,17 +50,30 @@ todoControl.addEventListener('submit', function(event){
     value: headerInput.value,
     completed: false
    };
- 
+
+  
+ //проверили на пустое значение, если НЕТ - добавили и обнулили, если ДА - то алерт
    if (headerInput.value !== '') {
     todoData.push(newData);
     headerInput.value = '';
   } else {
     alert('Введите задание');
   }  
+   
+  if (todoData === null) {
+    todoData = JSON.parse(localStorage.getItem("todoData"));
+  }
+
+ //сохраняем массив в виде JSON строки
+  localStorage.setItem(todoData, JSON.stringify(todoData));
 
   render();
 
  });
 
+//  document.addEventListener('DOMContentLoaded', function(){
+//   
+//   console.log('ok');
+// }) 
 
 render();
