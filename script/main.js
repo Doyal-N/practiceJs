d37757746e1f50d2ebc53889d0049e6110f50fac
Doyal-N <coin20@protonmail.com>
2020-04-26 19:28:16 +0500

    lesson_hard

--- script/main.js
+++ script/main.js
@@ -4,17 +4,18 @@ let date = new Date(2020, 3, 25),
     weekDay = [ 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье' ];
 
 function showWeekDay(date) {
-  return weekDay[date.getDay()].bold();
+  return weekDay[date.getDay()];
 }
 
 const today = showWeekDay(date);
-document.write(today + "<br \/>");
+
 
 weekDay.forEach(function(item){
-  if (item !== weekDay[date.getDay()]) {
-    item = item.italics();
-    document.write(item + "<br \/>");
+  if (item == weekDay[date.getDay()]) {
+    item = item.italics().bold();
+  } else if (item == weekDay[5] || item == weekDay[6]) {
+    item = item.italics()
   }
-  
+  document.write(item + "<br \/>");
 })
 
