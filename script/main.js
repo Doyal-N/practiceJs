c19988ef366c1f70abf5b93fc505fa8caa876650
Doyal-N <coin20@protonmail.com>
2020-04-26 16:42:58 +0500

    lesson_hard

--- script/main.js
+++ script/main.js
@@ -1,19 +1,20 @@
 'use strict';
 
-let arr = ["23", "45", "115", "67", "964", "35", "44", "12"];
-//перебираем массив и вычленяем строки с цифрой 2 и 4 (нестрогое равно)
-arr.forEach(function(item){
-  if (item.charAt(0) == 2 || item.charAt(0) == 4) {
-    console.log(item);
-  }
-});
-//простые числа от 1 до 100
-let n = 100;
+let date = new Date(2020, 3, 25),
+    weekDay = [ 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье' ];
 
-nextNumber:
-for ( let i = 1; i <= n; i++) {
-  for ( let b = 2; b < i; b++) {
-    if ( i % b == 0) continue nextNumber
-  } 
-  console.log('Делитель числа ' + i + ': ' + 1 + ' и ' + i);
+function showWeekDay(date) {
+  return weekDay[date.getDay()].bold();
 }
+
+const today = showWeekDay(date);
+document.write(today + "<br \/>");
+
+weekDay.forEach(function(item){
+  if (item !== weekDay[date.getDay()]) {
+    item = item.italics();
+    document.write(item + "<br \/>");
+  }
+  
+})
+
