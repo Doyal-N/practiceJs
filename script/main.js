eb0db581e59a3719ceabac4b2b137a51d638f181
Doyal-N <coin20@protonmail.com>
2020-04-26 19:54:09 +0500

    lesson hard

--- script/main.js
+++ script/main.js
@@ -1,15 +1,12 @@
 'use strict';
-
+//новый объект Дата и массив
 let date = new Date(2020, 3, 25),
     weekDay = [ 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье' ];
-
+//функция выдает номер дня, которое выдергивает день из массива
 function showWeekDay(date) {
   return weekDay[date.getDay()];
 }
-
-const today = showWeekDay(date);
-
-
+// перебираем массив и навещиваем свойства на отдельные дни
 weekDay.forEach(function(item){
   if (item == weekDay[date.getDay()]) {
     item = item.italics().bold();
