1ca138414337da475ea745866c288821ed5e88e3
Doyal-N <coin20@protonmail.com>
2020-04-28 17:18:51 +0500

    lesson_14_part2

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const DomElem = function() {
-    this.selector = '';
+    this.selector = '#sf';
     this.height = 0; 
     this.width = 0;
     this.bg = '';
@@ -9,11 +9,28 @@ const DomElem = function() {
 
 };
 
-
-
-
-
+DomElem.prototype.changeCondition = function() {
+ if (this.selector[0] === '.') {
+  let div = document.createElement('div');
+  div.className = 'box';
+  div.textContent = 'Это просто блок';
+  div.style.cssText=`background-color: green;
+  height: 200px;
+  width: 300px;
+  font-size: 22px;`; 
+  document.write(div);
+ } else if (this.selector[0] === '#') {
+   let text = document.createElement('p');
+   text.id = 'art';
+   text.textContent = 'Это параграф';
+   text.style.cssText= `background-color: grey;
+   height: 700px;
+   width: 200px;
+   font-size: 22px;`
+   document.write(text);
+ }
+};
 
 const domElem = new DomElem();
 
-console.log(domElem);
+DomElem.prototype.changeCondition();
