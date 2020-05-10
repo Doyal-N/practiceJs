bf5beb06725904dcccf6e17c59ca7f25d5b22521
Doyal-N <coin20@protonmail.com>
2020-05-10 20:55:14 +0500

    lesson_18

--- script/main.js
+++ script/main.js
@@ -1,20 +1,39 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function() {
   'use strict';
   
   //Timer
   function countTimer(deadline) {
-   let hours = document.querySelector('#timer-hours'),
-       minutes = document.querySelector('#timer-minutes'),
-       seconds = document.querySelector('#timer-seconds'),
-       dateStop = new Date(deadline).getTime(),
+   let timerHours = document.querySelector('#timer-hours'),
+       timerMinutes = document.querySelector('#timer-minutes'),
+       timerSeconds = document.querySelector('#timer-seconds');
+
+   function getTimeRemaining() {   
+   let dateStop = new Date(deadline).getTime(),
        dateNow = new Date().getTime(),
-       timeRemaining = dateStop - dateNow;
+       timeRemaining = (dateStop - dateNow) / 1000,
+       seconds = Math.floor(timeRemaining % 60),
+       minutes = Math.floor((timeRemaining / 60) % 60),
+       hours = Math.floor(timeRemaining / 3600);
+       return {timeRemaining, hours, minutes, seconds};
+      } 
 
-      console.log(timeRemaining);
+    function updateClock() {
+      let timer = getTimeRemaining();
 
-  }
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+     
+      if (timer.timeRemaining > 0) {
+       setTimeout (updateClock, 1000)
+     }
+    }  
 
-  countTimer('01 july 2020'); 
+     updateClock();
+    
+    }
+
+  countTimer('12 May 2020')
 
 
 
