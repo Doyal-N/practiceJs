7e79824435eae32d3332b226755e1bddbbcbfb90
Doyal-N <coin20@protonmail.com>
2020-05-10 09:30:40 +0500

    lesson_18

--- script/main.js
+++ script/main.js
@@ -1,5 +1,46 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', function(){
+  'use strict';
+  
+  //Timer
+  function countTimer(deadline) {
+   let hours = document.querySelector('#timer-hours'),
+       minutes = document.querySelector('#timer-minutes'),
+       seconds = document.querySelector('#timer-seconds'),
+       dateStop = new Date(deadline).getTime(),
+       dateNow = new Date().getTime(),
+       timeRemaining = dateStop - dateNow;
+
+      console.log(timeRemaining);
+
+  }
+
+  countTimer('01 july 2020'); 
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+})
