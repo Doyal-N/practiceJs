9be85b695c86036bb5c549e60dc3c271692065da
Doyal-N <coin20@protonmail.com>
2020-05-11 09:55:29 +0500

    lesson_18

--- script/main.js
+++ script/main.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', function() {
    let timerHours = document.querySelector('#timer-hours'),
        timerMinutes = document.querySelector('#timer-minutes'),
        timerSeconds = document.querySelector('#timer-seconds');
-
+  //получаем остаток времени вычисляя разницу
    function getTimeRemaining() {   
    let dateStop = new Date(deadline).getTime(),
        dateNow = new Date().getTime(),
@@ -15,8 +15,8 @@ window.addEventListener('DOMContentLoaded', function() {
        minutes = Math.floor((timeRemaining / 60) % 60),
        hours = Math.floor(timeRemaining / 3600);
        return {timeRemaining, hours, minutes, seconds};
-      } 
-
+      }; 
+  //отображаем на экране по заданному стандарту используя условия
     function updateClock() {
       let timer = getTimeRemaining();
 
@@ -24,16 +24,31 @@ window.addEventListener('DOMContentLoaded', function() {
       timerMinutes.textContent = timer.minutes;
       timerSeconds.textContent = timer.seconds;
      
-      if (timer.timeRemaining > 0) {
-       setTimeout (updateClock, 1000)
+      if (timer.timeRemaining < 0) {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
      }
-    }  
+
+      if (timer.seconds >= 0 && timer.seconds < 10) {
+        timerSeconds.textContent = '0' + timer.seconds
+      }
+     
+      if(timer.minutes >= 0 && timer.minutes < 10) {
+        timerMinutes.textContent = '0' + timer.minutes
+      }
+      
+      if(timer.hours >= 0 && timer.hours < 10) {
+        timerHours.textContent = '0' + timer.hours
+      }
+
+    };  
 
      updateClock();
     
     }
 
-  countTimer('12 May 2020')
+  setInterval(countTimer, 1000, '11 May 2020 19:00');
 
 
 
