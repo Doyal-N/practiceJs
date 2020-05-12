71081fff470b46b5c1a4abe874abda96f57779fa
Doyal-N <coin20@protonmail.com>
2020-05-12 23:51:17 +0500

    lesson_18_3

--- script/main.js
+++ script/main.js
@@ -6,10 +6,38 @@ document.addEventListener('DOMContentLoaded', function() {
     start =  document.querySelector('.start'),
     reset = document.querySelector('.reset');
 
+let count = 0;
+let animate;
 
+function goBox() {
+ animate = requestAnimationFrame(goBox)
+ count = count + 2;
+if (count < 570) {
+  box.style.top = `${count}px`;
+  } else {
+   cancelAnimationFrame(animate);
+ }
+   }
 
+let anima = true;   
 
+start.addEventListener('click', () => {
+if (anima) {
+  animate = requestAnimationFrame(goBox);
+  anima = false;
+} else {
+  anima = true;
+  cancelAnimationFrame(animate);
+ }
+  
+  });
 
+ reset.addEventListener('click', () => {
+  box.style.top = '-100px';
+  count = 0;
+  anima = true;
+  
+}) 
 
 
 
