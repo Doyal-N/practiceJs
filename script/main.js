8ae2881c67f2a3adf7ed2af1dbce4c3d9211fb24
Doyal-N <coin20@protonmail.com>
2020-05-12 21:37:49 +0500

    lesson_18_3

--- script/main.js
+++ script/main.js
@@ -1,26 +1,10 @@
-window.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
+
   'use strict';
   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ const box = document.querySelector('.box'),
+    start =  document.querySelector('.start'),
+    reset = document.querySelector('.reset');
 
 
 
