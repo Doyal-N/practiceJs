5fd28065555a96bee7d5c9328809100436bc37fc
Doyal-N <coin20@protonmail.com>
2020-05-14 12:24:06 +0500

    lesson_19_1

--- script/main.js
+++ script/main.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
   
-  //Timer
-const countTimer = (deadline) => {
+//Timer
+ const countTimer = (deadline) => {
    let timerHours = document.querySelector('#timer-hours'),
        timerMinutes = document.querySelector('#timer-minutes'),
        timerSeconds = document.querySelector('#timer-seconds'),
        interval;
   
-   //получаем остаток времени вычисляя разницу
+//получаем остаток времени вычисляя разницу
  const getTimeRemaining = () => { 
    let dateStop = new Date(deadline).getTime(),
        dateNow = new Date().getTime(),
@@ -21,7 +21,7 @@ const countTimer = (deadline) => {
        
       }; 
      
-  //отображаем на экране по заданному стандарту, используя условия
+//отображаем на экране по заданному стандарту, используя условия
  const updateClock = () => {
       let timer = getTimeRemaining();
 
@@ -45,18 +45,46 @@ const countTimer = (deadline) => {
  countTimer('14 may 2020 20:57')
 
 //Menu
-const toggleMenu = () => {
+ const toggleMenu = () => {
   const btnMenu = document.querySelector('.menu'),
-        menu = document.querySelector('menu');
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        menuItems = menu.querySelectorAll('ul>li');
 
-  btnMenu.addEventListener('click', () => {
-   menu.style.transform = 'translate(0)'
-  });      
+  //проверяет наличие свойства, добавление-удаление свойства
+  const handlerMenu = () => { menu.classList.toggle('active-menu') };     
 
+  //закрытие-открытие по кнопкам
+  btnMenu.addEventListener('click', handlerMenu); 
+  closeBtn.addEventListener('click', handlerMenu);
+
+  //закрытие-переход по пунктам меню (цикл либо перебор foreach)
+  for(let i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', handlerMenu);
+  }
 };
 
 toggleMenu();
 
+//Pop-up
+ const togglePopUp = () => {
+   const popup = document.querySelector('.popup'),
+   popupBtn = document.querySelectorAll('.popup-btn'),
+   popupCloseBtn = document.querySelector('.popup-close');
+
+   popupBtn.forEach((elem) =>{
+    elem.addEventListener('click', () => {
+     popup.style.display = 'block';
+    });
+   });
+
+   popupCloseBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+   });
+
+ };
+ 
+ togglePopUp();
 
 
 
