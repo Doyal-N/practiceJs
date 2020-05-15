89d6e979d4d0639b6d7e3409f28dfcbe14ac27af
Doyal-N <coin20@protonmail.com>
2020-05-15 12:22:10 +0500

    lesson smooth scroll

--- script/main.js
+++ script/main.js
@@ -126,6 +126,26 @@ toggleMenu();
 
  togglePopUp();
 
+//SCROLL
+const hrefs = document.querySelectorAll('.scroll'),
+scrollDown = document.querySelector('.scroll-down'),
+service = document.getElementById('service-block');
+
+//перебор листа и скролл по ссылкам меню
+ for (let i = 0; i < hrefs.length; i++) {
+   hrefs[i].addEventListener('click', () => {
+     event.preventDefault();
+
+   const blockID = event.target.getAttribute('href').substr(1);
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth', block: 'start'})
+   });
+ };
+//скролл по кнопке
+ scrollDown.addEventListener('click', () => {
+   event.preventDefault();
+   service.scrollIntoView({behavior: 'smooth', block: 'start'});
+ })
 
 
 
