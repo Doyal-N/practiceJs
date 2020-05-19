6a4ae1b2ea8b9684977d4acaa928a57ba9784786
Doyal-N <coin20@protonmail.com>
2020-05-19 17:55:48 +0500

    lesson_22

--- script/main.js
+++ script/main.js
@@ -325,8 +325,20 @@ slider.addEventListener('mouseout', (event) => {
  
  slider();
 
+//COMMAND
+ const images = document.querySelectorAll('.command__photo');
 
+ const changeImage = () => {
+  images.forEach((img) => {
+    const way = img.getAttribute('src');
+    const dataWay = img.dataset.img;
 
+    img.addEventListener('mouseenter', () => {img.src = dataWay})
+    img.addEventListener('mouseleave', () => {img.src = way})
+  });
+};
+
+changeImage();
 
 
 
