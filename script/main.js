38bb4870d63a100738d368329273b0fc03df5c37
Doyal-N <coin20@protonmail.com>
2020-05-21 14:11:33 +0500

    lesson_23

--- script/main.js
+++ script/main.js
@@ -340,12 +340,63 @@ const changeImage = () => {
 changeImage();
 
  //CALCULATOR
-const input = document.querySelectorAll('input[type=number]');
+const calc = (price = 100) => {
 
-input.forEach((item) => {
-  const num = /[^\d]+$/g;
-  item.value.replace(num, '');
-})
+ const calcBlock = document.querySelector('.calc-block'),
+  calcType = document.querySelector('.calc-type'),
+  calcSquare = document.querySelector('.calc-square'),
+  calcCount = document.querySelector('.calc-count'),
+  calcDay = document.querySelector('.calc-day'),
+  calcTotal = document.getElementById('total');
+
+  const countSum = () => {
+
+    let total = 0,
+    countValue = 1,
+    dayValue = 1;
+    const typeValue = calcType.options[calcType.selectedIndex].value,
+     square = calcSquare.value;
+
+    if (calcCount.value > 1) {
+     countValue += (calcCount.value - 1) / 10;
+     }
+     
+    if (calcDay.value && calcDay.value <= 5) {
+      dayValue *= 2
+    } else if (calcDay.value && calcDay.value < 10) {
+      dayValue *= 1.5
+    }
+
+    if (typeValue && square) {
+      total = price * typeValue * square * countValue * dayValue;
+    } 
+
+    calcTotal.textContent = total;
+
+  };
+ 
+  calcBlock.addEventListener('change', (event) => {
+    let target = event.target;
+
+    if (target === calcType || target === calcSquare || target === calcCount || target === calcDay) {
+      countSum();
+    }
+  });
+
+const inputNumbers = () => {
+  const input = document.querySelectorAll('input[type=number]');
+
+  input.forEach((item) => {
+    const num = /[^\d]+$/g;
+    item.value.replace(num, '');
+  });
+ };
+
+ inputNumbers();
+};
+
+calc(100);
+
 
 
 
