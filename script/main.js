731dce18641b3c29f4c0522fd49b3bb370e04e6b
Doyal-N <coin20@protonmail.com>
2020-05-26 23:12:30 +0500

    lesson_25

--- script/main.js
+++ script/main.js
@@ -397,6 +397,26 @@ const inputNumbers = () => {
 
 calc(100);
 
+//валидация полей
+ const validInputs = () => {
+  const nameInputs = document.querySelectorAll('input[name="user_name"], input[name="user_message"]'),
+  phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+  //разрешено кириллица и пробел
+  for (let i = 0; i < nameInputs.length; i++) {
+    nameInputs[i].addEventListener('input', () =>{
+      nameInputs[i].value = nameInputs[i].value.replace(/[a-zA-Z0-9_-`\\/.\-=+*]/g, '')
+    })
+  };
+ //разрешено цифры и +
+  for (let i = 0; i < phoneInputs.length; i++) {
+    phoneInputs[i].addEventListener('input', () =>{
+      phoneInputs[i].value = phoneInputs[i].value.replace(/[^\+\d]/g, '')
+    })
+  };
+     
+ };
+ validInputs();
+
 //send AJAX-form
 const sendForm = () => {
   const errorMsg = 'Что-то пошло не так...',
