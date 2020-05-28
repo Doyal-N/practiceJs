4812871f08176e9381095b78a3a8b4eae546ae1a
Doyal-N <coin20@protonmail.com>
2020-05-28 16:03:07 +0500

    lesson Promise practice

--- script/main.js
+++ script/main.js
@@ -2,28 +2,37 @@
 
 const out = document.getElementById('output');
 
-const getData = (url, outputData) => {
-  const request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.addEventListener('readystatechange', () => {
-    if (request.readyState !== 4) {
-      return;
-    }
-    if (request.status === 200) {
-      const response = JSON.parse(request.responseText);
-      outputData(response);
-    } else {
-     console.error(request.statusText);
-    }
+const getData = (url) => {
+
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        const response = JSON.parse(request.responseText);
+        resolve(response);
+      } else {
+       reject(request.statusText);
+      }
+    });
+    request.send();
   });
-  request.send();
+ 
 };
 
 const outputPhotos = (data) => {
  const random = Math.floor(Math.random() * data.length);
  const obj = data[random];
- out.innerHTML = `<h2>${obj.title}</h2>
-                  <img src='${obj.url}'>`
+ out.innerHTML = `<h3>${obj.title}</h3>
+                  <img src='${obj.thumbnailUrl}'>`
 };
 
-getData('https://jsonplaceholder.typicode.com/photos', outputPhotos);
+let url = 'https://jsonplaceholder.typicode.com/photos';
+
+getData(url)
+  .then(outputPhotos)
+  .catch(error => console.log(error));
+
