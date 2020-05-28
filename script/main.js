69efd129f59ffb7f4f59eaff56b7d50464736950
Doyal-N <coin20@protonmail.com>
2020-05-28 12:36:39 +0500

    learn promise lesson_26

--- script/main.js
+++ script/main.js
@@ -1,5 +1,29 @@
 'use strict';
 
+const out = document.getElementById('output');
 
+const getData = (url, outputData) => {
+  const request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.addEventListener('readystatechange', () => {
+    if (request.readyState !== 4) {
+      return;
+    }
+    if (request.status === 200) {
+      const response = JSON.parse(request.responseText);
+      outputData(response);
+    } else {
+     console.error(request.statusText);
+    }
+  });
+  request.send();
+};
 
-    
+const outputPhotos = (data) => {
+ const random = Math.floor(Math.random() * data.length);
+ const obj = data[random];
+ out.innerHTML = `<h2>${obj.title}</h2>
+                  <img src='${obj.url}'>`
+};
+
+getData('https://jsonplaceholder.typicode.com/photos', outputPhotos);
